89c008c200a197c925bde4ab7321a5790e9d6693
eug19 <evgenya9korol@gmail.com>
2025-08-12 20:21:32 +0200

    Homework #7
    please check my homework #7 when you have time :)
    so sorry for the delay

--- src/SuperHero.java
+++ src/SuperHero.java
@@ -1,4 +1,51 @@
-public class SuperHero {
+import java.util.Random;
 
-    // Тут мають бути властивості і поведінка Супергероя
+public class SuperHero {
+    private String name;
+    private int attackPower;
+    private int defensePower;
+    private int health;
+    private Random rand = new Random();
+
+    public SuperHero(String name, int attackPower, int defensePower) {
+        this.name = name;
+        this.attackPower = attackPower;
+        this.defensePower = defensePower;
+        this.health = 100;
+    }
+
+    public void attack(SuperHero opponent) {
+        if (!this.isAlive()) {
+            System.out.println(this.name + " не може атакувати, бо мертвий.");
+            return;
+        }
+
+        int[] possibleHits = {10, 15, 20, 25, 30};
+        int randomHit = possibleHits[rand.nextInt(possibleHits.length)];
+
+        System.out.println(this.name + " атакує " + opponent.name + " і завдає " + randomHit + " ушкоджень.");
+        opponent.takeDamage(randomHit);
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+        if (health < 0) health = 0;
+        System.out.println(name + " має залишок здоров'я: " + health);
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    public void showStats() {
+        System.out.println("Ім'я: " + name);
+        System.out.println("Сила атаки: " + attackPower);
+        System.out.println("Сила захисту: " + defensePower);
+        System.out.println("Здоров'я: " + health);
+        System.out.println("--------------------------");
+    }
+
+    public String getName() {
+        return name;
+    }
 }
